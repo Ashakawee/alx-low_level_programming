18ae574da6ee9b53b2e56f9f461031751f6ad7ac
Ashakawee <alashakawee@gmail.com>
2022-09-09 03:33:03 -0700

    task 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio>
+/**
+ * mainn - entery point
+ *
+ * return: always 0 (success)
+ *
+ * /
+ int main(void)
+ {
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	
+	}
