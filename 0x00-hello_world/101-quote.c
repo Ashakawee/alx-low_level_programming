bdea59d2d567ae738a2e4cb71bcee02d22065e1b
Ashakawee <alashakawee@gmail.com>
2022-09-11 13:45:31 -0700

    tsak 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <unistd.h>
+/*
+ * main entry
+ * return 1
+ */
+int main(void)
+{
+	write(1, "and that piec of art is usrful\"-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
