74b702d419b7176b7fcfea43ca6c53988dad295c
Ashakawee <alashakawee@gmail.com>
2022-09-12 07:02:13 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - print lower case alphabets to uppercase
+ *
+ * Return; 0
+ *
+ */
+int main(void)
+{
+	char ashaka[53] = "ABCDEFGHIJKLMNOPQRSTUVWXZQWERTYUOKJHGFSDXCVTYASHAKWEE";
+	int i;
+	for (i = 0; i < 53; i++)
+	{
+		putchar(ashaka[i]);
+	}
+	putchar('\n');
+	return (0);
+}
