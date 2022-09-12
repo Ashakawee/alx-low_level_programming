3b36512295daaa7b1fa71f382ee1ca9b4fc32cca
Ashakawee <alashakawee@gmail.com>
2022-09-12 04:45:37 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-/*
+/**
  * main - main block
- * Description: using the main function
+ *
  * Return: 0
+ *
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
