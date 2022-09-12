b554d8f3ee2bd628e7aace1b3a27af018b3a69bd
Ashakawee <alashakawee@gmail.com>
2022-09-12 07:21:01 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing lower and upper case
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char musa[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n;
+
+	for (n = 0; n < 52; n++)
+	{
+		putchar(musa[i]);
+	}
+	putcahr('\n');
+	return (0);
+}
