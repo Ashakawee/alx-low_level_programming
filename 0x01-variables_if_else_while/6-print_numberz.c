5e0945c6b613fef09361713fd3eeffe257785ce4
Ashakawee <alashakawee@gmail.com>
2022-09-13 05:36:27 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing and a base ten numbers
+ *
+ * Return: 0 always (success)
+ *
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+	return (0);
+}
