ce07eee87dd20ba097105c243cb0c8dc6d70048a
Ashakawee <alashakawee@gmail.com>
2022-09-13 01:51:40 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing lower case alphabents exept q and e
+ *
+ * Return: 0 always  (success)
+ *
+ */
+int main(void)
+{
+	char ashk[22] = "abcdfghijklmnoprstuvwxy";
+	int m;
+
+	for (m = 0; m < 22; m++)
+	{
+		putchar(ashk[m]);
+	}
+	putchar('\n');
+	return (0);
+}
