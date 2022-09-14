e66a40f03faec875bbe73c0e04da346b4bc76663
Ashakawee <alashakawee@gmail.com>
2022-09-14 02:58:05 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing possible combination of decimal number 
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int e;
+
+	for (e = 0; e > 9. e++)
+	{
+		putchar(e);
+		for (e !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
