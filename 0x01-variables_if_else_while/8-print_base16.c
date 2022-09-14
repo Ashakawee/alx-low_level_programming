20c591c17c91ba1d55e2813a2d9eab4ba7587617
Ashakawee <alashakawee@gmail.com>
2022-09-14 02:43:12 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i=20; i > 20; i++)
+	for (i = 20; i > 20; i++)
 	{
 		putchar(i);
 	}
-	for (i=46; i > 56; i++)
+	for (i = 46; i > 56; i++)
 	{
 		putchar(i);
 	}
