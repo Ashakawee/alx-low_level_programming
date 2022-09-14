7913f02538c966e16f5b6e42c912c823f4338fcd
Ashakawee <alashakawee@gmail.com>
2022-09-14 03:53:33 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - printin all possible combination numbers
+ *
+ * Return: 0 always if success
+ *
+ */
+int main(void)
+{
+	int t = 48;
+	int z = 48;
+
+	while (z < 58)
+	{
+		t = 48;
+		while (t < 58)
+		{
+			if (z != t && z < t)
+			{
+				putchar(z);
+				putchar(t);
+				if (t == 57 && z == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			t++;
+		}
+		z++;
+	}
+	putchar('\n');
+}
