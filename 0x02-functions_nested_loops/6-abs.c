e8b994ca9d05769395d0b16f91b117d6dba5e988
Ashakawee <alashakawee@gmail.com>
2022-09-16 05:53:35 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Check Holberton
+ * @r: An integre input
+ * Return: absolut value of r
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
