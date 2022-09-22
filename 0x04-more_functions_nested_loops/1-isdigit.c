9e3f13d7ae442ef344cfdeba2147f27cb97d12fb
Ashakawee <alashakawee@gmail.com>
2022-09-22 04:46:39 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,9 +9,12 @@
 
 int _isdigit(int c)
 {
-	c = '0';
-	printf("%c: %d\n", c);
-	c = 'a';
-	printf("%c: %d\n", c);
-	return (0);
+	if ( c <= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
