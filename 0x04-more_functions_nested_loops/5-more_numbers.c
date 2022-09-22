27d735e98ccb44a6e3356dfe39f3e4b85727820f
Ashakawee <alashakawee@gmail.com>
2022-09-22 06:30:36 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* more_numbers - printing a numbers between 0 to 14 ten times
+* Return: sucess always
+*/
+
+void more_numbers(void)
+{
+	int i, t;
+
+	while (i = '0'; i <= '9')
+	{
+		for (t = '0'; t <= '14'; t++)
+		{
+			_putchat(t);
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
