8fd0feaa9ed5368d38e288b451d2f5147846c714
Ashakawee <alashakawee@gmail.com>
2022-09-22 07:27:03 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 {
 	int i, t;
 
-	for (i = 0; i <= 9)
+	i = 0;
+	while (i < 10)
 	{
-		for (t = 0; t <= 14; t++)
+		for (t = 0; t < 15; t++)
 		{
-			if (t >= 10)
-				_putchat((t / 10) + 48);
-				_putchar((t % 10) + 48);
+			_putchat("%d", t);
 		}
 		_putchar('\n');
 	}
+	_putchar('\n');	
 }
