41d389f38911d51c7ec0daad22342b9719052bf2
Ashakawee <alashakawee@gmail.com>
2022-09-22 04:00:40 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isupper - cheack if parameter is an uppercase character
+*
+* @c: input character
+* Return: 1 if an uppercase character 0 in other
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
