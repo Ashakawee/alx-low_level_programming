e6b1730ec4354aa1c9fa1b67b6986e00ce56f387
Ashakawee <alashakawee@gmail.com>
2022-09-22 04:38:19 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,11 +7,11 @@
 * Return: 1 if c in digit 0 for other
 */
 
-int _isdigit(int c)
+int main(void)
 {
 	c = '0';
-	printf("%c: %d\n", c);
+	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
-	printf("%c: %d\n", c);
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
