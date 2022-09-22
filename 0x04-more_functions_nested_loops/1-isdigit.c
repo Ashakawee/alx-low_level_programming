05af08b2c5d00d05aa6c49ce84a6a04ddf0d8bd7
Ashakawee <alashakawee@gmail.com>
2022-09-22 04:16:54 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - cheking a digit between 0 t0 9
+* @c: cheking if c is chracter or digit
+* Return: 1 if c in digit 0 for other
+*/
+
+int _isdigit(int c)
+{
+	if (c = '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
