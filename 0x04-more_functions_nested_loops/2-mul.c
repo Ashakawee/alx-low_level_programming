4c4c2bb1db5ffa8ee9372d83999097c8b6bc21fa
Ashakawee <alashakawee@gmail.com>
2022-09-22 05:36:03 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - cheaking mul variables
+* @a: input variable
+* @b: input variable
+* Return: varible
+*/
+
+int mul(int a, int b)
+{
+	int multiple;
+	multiple = a * b;
+	return (multiple);
+}
