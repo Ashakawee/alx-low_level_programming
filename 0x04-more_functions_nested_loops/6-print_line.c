f63ff4f2c5fa8ebd7d01b7e7a6692cf65e5ba83d
Ashakawee <alashakawee@gmail.com>
2022-09-22 22:39:10 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - printing a straight line
+* @n: number of times for character
+* Return: seccess always 0
+*/
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		for (; i < n; i++)
+		_putchar('_');
+	}
+}
