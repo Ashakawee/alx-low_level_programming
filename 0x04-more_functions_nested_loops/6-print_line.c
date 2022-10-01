edc9235280b6d91c749dc9ec6eee159623ce4031
Ashakawee <alashakawee@gmail.com>
2022-10-01 07:27:59 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,12 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n < 0)
+	if (n <= 0)	
+		_putchar('\n');
+	else
 	{
-		for (; i < n; i++)
-		_putchar('_');
+		for (i = 1; i <= n; ++i)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
